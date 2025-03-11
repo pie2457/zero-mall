40c6752743412e63ccbf42cea44ce98e0e022d92
pie <yejinh_h@naver.com>
2025-03-11 16:56:15 +0900

    settings: 프로젝트 초기 설정

---
+++ src/main/java/com/project/zeromall/ZeroMallApplication.java
@@ -0,0 +1,13 @@
+package com.project.zeromall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroMallApplication.class, args);
+    }
+
+}
